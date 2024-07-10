6ef8b1e984726a970cb5f7ffbc63c1c95db34a9f
ehetshamshaukat <ehetsham.s@gmail.com>
2024-07-10 11:43:01 +0500

    prediction pipeline complete

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,7 @@ def save_file_as_pickle(path,obj_name):
     with open(path,"wb") as path:
         pickle.dump(obj_name,path)
         print("\t\t\t\t\t\t\tpickle file save")
+
+def load_pickle_file(obj_path):
+    with open(obj_path,"rb") as path:
+        return pickle.load(path)
